32ca6bf412a7a7d55ed7161d00bec3dca7637987
Kyrad1 <flealsaez@gmail.com>
2025-05-07 20:39:25 -0400

    intentando arreglarlo

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -9,3 +10,8 @@ def read_root():
 @app.get("/saludo/{nombre}")
 def saludar(nombre: str):
     return {"saludo": f"Hola, {nombre}. Bienvenido a la API"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
